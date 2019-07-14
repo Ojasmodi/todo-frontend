312ef34b75178b381cd38cc4bd4e4a06241b88ca
Ojas Modi <ojasmodi555@gmail.com>
2019-07-14 23:30:24 +0530

    pre3-final-commit

--- src/app/todo/dashboard/todo-mgmt.service.ts
+++ src/app/todo/dashboard/todo-mgmt.service.ts
@@ -9,7 +9,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class TodoMgmtService {
 
-  private url = 'http://localhost:3006';
+  private url = 'http://todoback.myinfo.world';
   private socket;
 
   constructor(public http: HttpClient, private cookieService: CookieService) {
